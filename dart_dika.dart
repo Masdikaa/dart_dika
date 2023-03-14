d579cfb78108633cd38a6e9801516027c0e0d4f6
Masdikaa <masdikailhanmansiz@gmail.com>
2023-03-14 08:45:02 +0700

    dart variables

---
+++ dart_dika.dart
@@ -0,0 +1,99 @@
+void main() {
+  dartVariable();
+  dartNumberVariable();
+}
+
+void dartVariable() {
+  // Dart Variables
+  // TIpeData namaVariabel
+  print("Normal variable");
+  String fullName = "Masdika Ilhan Mansiz";
+  print(fullName);
+
+  print("\nVar keyword");
+  varKeyword();
+
+  print("\nFinal Keyword");
+  finalKeyword();
+
+  print("\nConst Keyword");
+  constKeyword();
+
+  print("\nLate Keyword");
+  lateKeyword();
+}
+
+void varKeyword() {
+  // Membuat variabel langsung dengan nilainya menggunakan kata kunci var
+  // var namaVariabel = value
+  var umur = 19;
+  var nama = "Dika";
+
+  print("var nama = " + nama + "\nvar umur = " + umur.toString());
+}
+
+void finalKeyword() {
+  // variable yang tidak bisa di ubah valuenya
+  // final namaVariable = value;
+  var bapak = "Joko";
+  final ibu = "Surti";
+  print("Bapak = " + bapak);
+  print("Ibu = " + ibu);
+
+  // Mengisi ulang value dari variable
+  bapak = "Tejo";
+  // ibu = "Mamah";
+  print("Bapak = " + bapak);
+  print("Ibu = " + ibu);
+}
+
+void constKeyword() {
+  // FInal membuat variabel tidak dapat di deklarasikan ulang tapi bisa di ubah nilainya
+  // const membuat variable menjadi immuttable / tak dapat di ubah sama sekali
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[1] = 10; // Value dari array bisa di ubah
+  //array2[1] = 10; // error kode -> UnmodifiableListMixin.[]=test\dart_dika.dart
+
+  print(array1[1]);
+}
+
+void lateKeyword() {
+  // late digunakan untuk menunda deklarasi hingga variable di akses
+  // Jika variable tidak di akses maka tidak akan di jalankan
+  // tanpa late
+  var value = getValue();
+  print("Variable sudah dibuat");
+  print(value);
+
+  // dengan late
+  late var value1 = getValue();
+  print("Variable sudah dibuat");
+  print(value1);
+}
+
+//String Func
+String getValue() {
+  print("getValue() dipanggil");
+  return "Masdika";
+}
+
+void dartNumberVariable() {
+  // Dalam dart terdapat 2 tipe data number -> int dan double
+  print("\nNum keyword");
+  numKeyword();
+}
+
+void numKeyword() {
+  // num digunakan untuk membuat variabel number bertipe int dan double
+  // num namaVariable = value
+  int a = 10;
+  double b = 10.5;
+
+  num c = a;
+  num d = b;
+
+  print("num int = " + c.toString());
+  print("num double = " + d.toString());
+}
